bff3a59bd6f6fc69ed9450ad164db66a7b4ee867
Yogosun <118680859+Yogosun@users.noreply.github.com>
2024-11-02 16:55:28 +0300

    clone HomeworkAA

---
+++ HomeworkAA/Entity/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeworkAA.Entity
+{
+    public abstract class MenuItem
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+
+        protected MenuItem(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString() => $"{Name} - {Price} руб.";
+    }
+}
---
+++ HomeworkAA/Menus/Menu.cs
@@ -0,0 +1,28 @@
+﻿using HomeworkAA.Entity;
+using HomeworkAA.Menus.Food;
+using System.Collections.Generic;
+
+namespace HomeworkAA.Menus
+{
+    public class Menu
+    {
+        public List<MenuItem> Items { get; }
+
+        public Menu()
+        {
+            Items = new List<MenuItem>
+            {
+                new Pizza(1),
+                new Burger(2),
+                new Sushi(3),
+                new Pasta(4),
+                new Salad(5)
+            };
+        }
+
+        public MenuItem GetMenuItemById(int id)
+        {
+            return Items.Find(item => item.Id == id);
+        }
+    }
+}
